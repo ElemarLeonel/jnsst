47a47aa80b32d11607348cc99fd8ee0eebe5c8f1
Elemar-18 <elemarleonelbalduino@gmail.com>
2022-05-25 18:16:50 -0400

    Iniciando o formulário da CAT

--- src/components/FormCAT/index.js
+++ src/components/FormCAT/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import * as React from 'react';
 
-import { Box, Typography } from '@mui/material';
+import {
+    Box,
+    Typography,
+    FormControl,
+    TextField,
+} from '@mui/material';
+
+import './index.css';
 
 
-export default function FormCAT(){
-    return(
-        <Box>
-            <Typography>
-                Testando
-            </Typography>
+export default function FormCAT() {
+    return (
+        <Box className="box-form-cat">
+            <Box className="title-cat">
+                <Typography variant="h5" component="h5" color="secondary"
+                    fontWeight="bold">
+                    Formulário da CAT
+                </Typography>
+            </Box>
+            <FormControl className="form-cat" component="section">
+                <TextField id="emailEmpresa" label="Email"
+                    variant="outlined" color="secondary" required={true} />
+                <TextField id="telefoneEmpresa" label="Telefone"
+                    variant="outlined" color="secondary" required={true} />
+                <TextField id="CNPJouCAEPF" label="CNPJ / CAEPF"
+                    variant="outlined" color="secondary" required={true} />
+                <TextField id="razaoSocial" label="Razão Social ou Nome Fazenda"
+                    variant="outlined" color="secondary" required={true} />
+                <TextField id="razaoSocial" label="Teste"
+                    variant="outlined" color="secondary" required={true} />
+            </FormControl>
         </Box>
     )
 }
