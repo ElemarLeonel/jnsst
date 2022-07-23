c7318402ee3ea32b39a9ee20807b1fc777c2fd86
Elemar-18 <elemarleonelbalduino@gmail.com>
2022-07-23 11:14:32 -0400

    Adicionando modulo para envio de emails

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -1,8 +1,45 @@
-import { Box, Container, TextField, Typography, Button } from '@mui/material';
+import {
+    Box,
+    Container,
+    TextField,
+    Typography,
+    Button,
+    Link
+} from '@mui/material';
 import * as React from 'react';
 import './index.css';
+import axios from 'axios';
 
 export default function Contato() {
+    const [campos, setCampos] = React.useState({
+        fullname: '',
+        email: '',
+        title: '',
+        message: ''
+    });
+    
+    function handleInputChange(event){
+        campos[event.target.name] = event.target.value;
+        setCampos(campos);
+    }
+
+    function handleFormSubmit(event){
+        event.preventDefault();
+        console.log(campos);
+        send();
+    }
+
+    function send(){
+        const formData = new FormData();
+        Object.keys(campos).forEach(key => formData.append(key, campos[key]));
+        axios.post("http://localhost:4000/email", formData, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => alert(response.data));
+    }
+
     return (
         <Box className="box-main" component="section" id="contact">
             <Container maxWidth="lg" className="main-container">
@@ -19,20 +56,40 @@ export default function Contato() {
                     </Typography>
                     <Typography variant="p_subtitles" component="p"
                         className="subtitle-text-contact" color="secondary">
-                        Telefone Recepção: (69) 3451-8182 (Fixo) e (69) 98400-8182 (WhatsApp)
+                        Telefone Fixo Recepção: (69) 3451-8182
+                    </Typography>
+                    <Typography variant="p_subtitles" component="p"
+                        className="subtitle-text-contact" color="secondary">
+                        <Link color="secondary"
+                            href="https://api.whatsapp.com/send?phone=5569984008182&text=Oi.">
+                            WhatsApp Recepção: (69) 98400-8182
+                        </Link>
+                    </Typography>
+                    <Typography variant="p_subtitles" component="p"
+                        className="subtitle-text-contact" color="secondary">
+                        <Link color="secondary"
+                            href="https://api.whatsapp.com/send?phone=5569984008182&text=Oi.">
+                            WhatsApp eSocial: (69) 98485-8181
+                        </Link>
                     </Typography>
                 </Box>
-                <Box component="section" className="main-contact">
+                <Box component="form" className="main-contact" onSubmit={handleFormSubmit}>
                     <Box component="section" className="name-and-email">
                         <TextField id="full-name" label="Nome completo" variant="outlined"
-                            fullWidth color="secondary" />
+                            fullWidth color="secondary" required onChange={handleInputChange}
+                            name="fullname"/>
                         <TextField id="email" label="Email" variant="outlined"
-                            fullWidth color="secondary" />
+                            fullWidth color="secondary" required onChange={handleInputChange}
+                            name="email"/>
                     </Box>
+                    <TextField id="title" label="Título" variant="outlined"
+                            fullWidth color="secondary" required onChange={handleInputChange}
+                            name="title"/>
                     <TextField id="outlined-multiline-static" label="Mensagem"
-                        multiline maxRows={5} fullWidth color="secondary" />
+                        multiline maxRows={5} fullWidth color="secondary" required
+                        onChange={handleInputChange} name="message"/>
                     <Button variant="contained" color="secondary" maxWidth="md"
-                        size="large">Enviar</Button>
+                        size="large" type="submit">Enviar</Button>
                 </Box>
             </Container>
         </Box>
