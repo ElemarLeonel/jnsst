0b1f435a0188f2fb9fc67bccd3a4cac9936298f4
Elemar-18 <elemarleonelbalduino@gmail.com>
2022-10-27 11:26:15 -0400

    Adicionando foto do colaborador Cleiso e realizando ajustes na página inicial

--- src/components/Equipe/index.js
+++ src/components/Equipe/index.js
@@ -18,6 +18,7 @@ import ImagemMember2 from '../../assets/team/member-2.webp';
 import ImagemFlavia from '../../assets/team/flavia.webp';
 import ImagemElemar from '../../assets/team/elemar.webp';
 import ImagemJonathan from '../../assets/team/jonathan.webp';
+import ImagemCleiso from '../../assets/team/cleiso.webp';
 
 import InstagramIcon from '@mui/icons-material/Instagram';
 
@@ -26,77 +27,88 @@ const equipe = [
         nome: "Flávia Medeiros",
         profissao: "Técnico(a) em Segurança do Trabalho",
         imagem: ImagemFlavia,
-        descricao: "Membro da equipe da JN Saúde e Segurança do Trabalho - Flávia Medeiros",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Flávia Medeiros",
+        bio: "###",
         instagram: "https://www.instagram.com/flavia.medeiros.505/"
     },
     {
         nome: "Elemar Leonel",
         profissao: "Técnico em Informática",
         imagem: ImagemElemar,
-        descricao: "Membro da equipe da JN Saúde e Segurança do Trabalho - Elemar Leonel",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Elemar Leonel",
+        bio: "Graduado em Sistemas de Informação pela UNESC, atuando no Núcleo de Transmissão ao eSocial, realizando os envios dos eventos e prestando suporte técnico na área de informática.",
         instagram: "https://www.instagram.com/dev.elemarleonel/"
     },
     {
         nome: "Jonathan Alves",
         profissao: "Eng. de Segurança do Trabalho",
         imagem: ImagemJonathan,
-        descricao: "Membro da equipe da JN Saúde e Segurança do Trabalho - Jonathan Alves",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Jonathan Alves",
+        bio: "###",
         instagram: "https://www.instagram.com/jonathan_alvespb/"
     },
     {
         nome: "Odair Calisto",
         profissao: "Técnico(a) em Segurança do Trabalho",
         imagem: ImagemMember1,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Odair Calisto",
+        bio: "###",
         instagram: "#"
     },
     {
         nome: "Dener de Assis",
         profissao: "Técnico(a) em Segurança do Trabalho",
         imagem: ImagemMember2,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Dener de Assis",
+        bio: "###",
         instagram: "#"
     },
     {
         nome: "Cleiso Rafael",
         profissao: "Técnico(a) em Segurança do Trabalho",
-        imagem: ImagemMember1,
-        descricao: "###",
-        instagram: "#"
+        imagem: ImagemCleiso,
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Cleiso Rafael",
+        bio: "###",
+        instagram: "https://www.instagram.com/engcleisorafael/"
     },
     {
         nome: "Sônia Lagácio",
-        profissao: "Técnico em Enfermagem",
+        profissao: "Psicólogo(a)",
         imagem: ImagemMember1,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Sônia Lagácio",
+        bio: "###",
         instagram: "#"
     },
     {
         nome: "Maria Márcia",
         profissao: "Secretária",
         imagem: ImagemMember2,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Maria Márcia",
+        bio: "###",
         instagram: "#"
     },
     {
         nome: "Edileusa Lagácio",
         profissao: "Psicólogo(a)",
         imagem: ImagemMember1,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Edileusa Lagácio",
+        bio: "###",
         instagram: "#",
     },
     {
         nome: "Elaine Souza",
         profissao: "Secretária",
         imagem: ImagemMember2,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Elaine Souza",
+        bio: "###",
         instagram: "#"
     },
     {
         nome: "Dr. Antônio Gomes",
         profissao: "Fonoaudiólogo",
         imagem: ImagemMember2,
-        descricao: "###",
+        alt: "Membro da equipe da JN Saúde e Segurança do Trabalho - Antônio Gomes",
+        bio: "###",
         instagram: "#"
     }
 ]
@@ -107,26 +119,30 @@ export default function Equipe() {
         <Typography className="title-team" variant="h3_title" component="h3">EQUIPE</Typography>
         <Box className="box-row">
         {equipe.map((item) => (
-        <Card variant="outlined" className="cards"
-            sx={{ boxShadow: 2 }}>
+        <Card variant="outlined" className="cards">
             <CardMedia
                 component="img"
                 width="400"
                 height="400"
                 image={item.imagem}
-                alt={item.descricao}
+                alt={item.alt}
             />
             <CardContent>
                 <Typography gutterBottom variant="h4_title" component="h4" color="secondary">
                     {item.nome}
                 </Typography>
-                <Typography variant="p_description" component="p" color="secondary">
+                <Typography variant="h5_title" component="h5" color="secondary">
                     {item.profissao}
                 </Typography>
+                {/* <Typography variant="p_description" component="p" color="secondary"
+                    className="bio-description">
+                    {item.bio}
+                </Typography> */}
             </CardContent>
             <CardActions className="buttons-card">
                 <Button size="medium" 
-                href={item.instagram} 
+                href={item.instagram}
+                fullWidth={true}
                 variant="outlined" 
                 color="secondary" 
                 startIcon={<InstagramIcon />}>
