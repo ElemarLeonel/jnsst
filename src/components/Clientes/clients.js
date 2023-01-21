faf269f79b83a9d092f3065001a2f3c50edd1c47
magnojunior07 <magnojunior319@gmail.com>
2023-01-21 11:32:10 -0400

    add clients section

---
+++ src/components/Clientes/clients.js
@@ -0,0 +1,78 @@
+import logoPostoSaoRoque from "../../assets/clients/AUTO-POSTO-SÃO-ROQUE.jpg";
+import logoCastilho from "../../assets/clients/CASTILHO.png"
+import logoConcreaco from "../../assets/clients/CONCREAÇO.png";
+import logoConeSul from "../../assets/clients/CONE-SUL.png";
+import logoEletrogoes from "../../assets/clients/ELETROGOES.png";
+import logoFabrilar from "../../assets/clients/FABRILAR.png";
+import logoKrause from "../../assets/clients/FRIGOKRAUSE.png";
+import logoMazzutti from "../../assets/clients/GRUPO-MAZZUTTI.JPG"
+import logoIdealMoveis from "../../assets/clients/IDEAL-MÓVEIS.jpg";
+import logoInviolavel from "../../assets/clients/INVIOLÁVEL.png";
+import logoLindagua from "../../assets/clients/LIND'ÁGUA.jpg";
+import logoNBS from "../../assets/clients/NBS.png";
+import logoOlla from "../../assets/clients/OLLA.png";
+import logoPiemon from "../../assets/clients/PIEMON-ATACAREJO.png";
+import logoSupermercadoCristal from "../../assets/clients/SUPERMERCADO-CRISTAL.jpg";
+
+export const clients = [
+  {
+    nome: "Auto Posto São Roque",
+    logo: logoPostoSaoRoque
+  },
+  {
+    nome: "Grupo Castilho",
+    logo: logoCastilho
+  },
+  {
+    nome: "Grupo Concreaço",
+    logo: logoConcreaco
+  },
+  {
+    nome: "Cone Sul",
+    logo: logoConeSul
+  },
+  {
+    nome: "Eletrogoes",
+    logo: logoEletrogoes
+  },
+  {
+    nome: "Frabrilar",
+    logo: logoFabrilar
+  },
+  {
+    nome: "Frigorífico Krause",
+    logo: logoKrause
+  },
+  {
+    nome: "Grupo Mazzutti",
+    logo: logoMazzutti
+  },
+  {
+    nome: "Ideal Móveis",
+    logo: logoIdealMoveis
+  },
+  {
+    nome: "Inviolável",
+    logo: logoInviolavel
+  },
+  {
+    nome: "Grupo Lind'água",
+    logo: logoLindagua
+  },
+  {
+    nome: "Grupo NBS",
+    logo: logoNBS
+  },
+  {
+    nome: "Grupo Ollá Telecom",
+    logo: logoOlla
+  },
+  {
+    nome: "Grupo Piemon",
+    logo: logoPiemon
+  },
+  {
+    nome: "Supermercado Cristal",
+    logo: logoSupermercadoCristal
+  },
+]
