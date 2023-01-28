b87d25e04bff866e5492ba39d6a8834e649a1ee9
Elemar-18 <elemarleonelbalduino@gmail.com>
2023-01-28 10:12:24 -0400

    Atualizando imagens

--- src/components/Carrossel/index.js
+++ src/components/Carrossel/index.js
@@ -1,48 +1,14 @@
 import { Carousel } from 'react-carousel-minimal';
-import ImagemAbrilVerdeCastilho from '../../assets/imgs-carousel/acao_abril_verde_castilho.webp';
-import ImagemCursoEmpilhadeiraCairu from '../../assets/imgs-carousel/curso_empilhadeira_cairu.webp';
-import ImagemCursoPrimeirosSocorrosPiarara from '../../assets/imgs-carousel/curso_primeirossocorros_piarara.webp';
-import ImagemOutubroRosaEletrogoes from '../../assets/imgs-carousel/outubro-rosa-eletrogoes.webp';
-import ImagemNovembroAzulEletrogoes from '../../assets/imgs-carousel/novembro-azul-eletrogoes.webp';
+import { imgsCarousel, captionStyle, slideNumberStyle } from './carousel';
 import './index.css';
 
 export default function Carrossel() {
- const data = [
-    {
-      image: ImagemAbrilVerdeCastilho,
-      caption: "Ação Abril Verde Castilho"
-    },
-    {
-      image: ImagemCursoEmpilhadeiraCairu,
-      caption: "Treinamento de Empilhadeira Cairu Bicicletas"
-    },
-    {
-      image: ImagemCursoPrimeirosSocorrosPiarara,
-      caption: "Treinamento de Primeiros Socorros Piarara Alimentos"
-    },
-    {
-      image: ImagemOutubroRosaEletrogoes,
-      caption: "Ação Sobre Outubro Rosa na Eletrogoes"
-    },
-    {
-      image: ImagemNovembroAzulEletrogoes,
-      caption: "Ação Sobre Novembro Azul na Eletrogoes"
-    }
-  ];
-
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 600,
-  }
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 600,
-  }
+ 
   return (
     <section className="carousel">
       <div className="carousel-subitem" style={{ textAlign: "center" }}>
           <Carousel
-            data={data}
+            data={imgsCarousel}
             time={4000}
             width="100%"
             height="500px"
